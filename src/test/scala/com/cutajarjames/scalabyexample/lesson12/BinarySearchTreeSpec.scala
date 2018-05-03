e5240b7304764ee75b95c76e2bddf911cf990b94
cutajarj2 <james.cutajar@blip.pt>
2018-05-03 14:57:55 +0100

    Fixing lesson 12 test

--- src/test/scala/com/cutajarjames/scalabyexample/lesson12/BinarySearchTreeSpec.scala
+++ src/test/scala/com/cutajarjames/scalabyexample/lesson12/BinarySearchTreeSpec.scala
@@ -4,57 +4,57 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class BinarySearchTreeSpec extends FlatSpec with Matchers {
   "A BinarySearchTree" should "output None if not found" in {
-    //val binarySearchTree:BinarySearchTree[Int,String] = BinarySearchTree(5,"James")
+    //val binarySearchTree: BinarySearchTree[Int, String] = UnBalancedBinarySearchTree(5, "James")
     val binarySearchTree:BinarySearchTree[Int,String] = null
     binarySearchTree.search(10) shouldEqual None
   }
 
-  "A BinarySearchTree" should "output Some if not found" in {
-    //val binarySearchTree:BinarySearchTree[Int,String] = BinarySearchTree(5,"James")
+  "A BinarySearchTree" should "output the correct value" in {
+    //val binarySearchTree: BinarySearchTree[Int, String] = UnBalancedBinarySearchTree(5, "James")
     val binarySearchTree:BinarySearchTree[Int,String] = null
-    binarySearchTree.insert(4,"Isabel")
-    binarySearchTree.insert(8,"Michelle")
-    binarySearchTree.insert(10,"Ruth")
-    binarySearchTree.search(10) should contain("Ruth")
+    binarySearchTree.insert(4, "Isabel")
+      .insert(8, "Michelle")
+      .insert(10, "Ruth")
+      .search(10) should contain("Ruth")
   }
 
   "A BinarySearchTree" should "output correct return for all searches" in {
-    //val binarySearchTree:BinarySearchTree[Int,String] = BinarySearchTree(5,"James")
+    //val binarySearchTree: BinarySearchTree[Int, String] = UnBalancedBinarySearchTree(5, "James")
     val binarySearchTree:BinarySearchTree[Int,String] = null
-    binarySearchTree.insert(4,"Isabel")
-    binarySearchTree.insert(8,"Michelle")
-    binarySearchTree.insert(10,"Ruth")
-    binarySearchTree.insert(15,"Judith")
-    binarySearchTree.insert(7,"Sam")
-    binarySearchTree.insert(9,"John")
-    binarySearchTree.insert(6,"Matti")
-    binarySearchTree.insert(12,"Jane")
-    binarySearchTree.insert(20,"Sarah")
-    binarySearchTree.insert(14,"Peter")
-    binarySearchTree.insert(17,"Joe")
+    val binarySearchTreeAfterInsert = binarySearchTree.insert(4, "Isabel")
+      .insert(8, "Michelle")
+      .insert(10, "Ruth")
+      .insert(15, "Judith")
+      .insert(7, "Sam")
+      .insert(9, "John")
+      .insert(6, "Matti")
+      .insert(12, "Jane")
+      .insert(20, "Sarah")
+      .insert(14, "Peter")
+      .insert(17, "Joe")
 
-    binarySearchTree.search(4) should contain("Isabel")
-    binarySearchTree.search(8) should contain("Michelle")
-    binarySearchTree.search(10) should contain("Ruth")
-    binarySearchTree.search(15) should contain("Judith")
-    binarySearchTree.search(7) should contain("Sam")
-    binarySearchTree.search(9) should contain("John")
-    binarySearchTree.search(6) should contain("Matti")
-    binarySearchTree.search(12) should contain("Jane")
-    binarySearchTree.search(20) should contain("Sarah")
-    binarySearchTree.search(14) should contain("Peter")
-    binarySearchTree.search(17) should contain("Joe")
-    binarySearchTree.search(34) shouldEqual None
+    binarySearchTreeAfterInsert.search(4) should contain("Isabel")
+    binarySearchTreeAfterInsert.search(8) should contain("Michelle")
+    binarySearchTreeAfterInsert.search(15) should contain("Judith")
+    binarySearchTreeAfterInsert.search(10) should contain("Ruth")
+    binarySearchTreeAfterInsert.search(7) should contain("Sam")
+    binarySearchTreeAfterInsert.search(9) should contain("John")
+    binarySearchTreeAfterInsert.search(6) should contain("Matti")
+    binarySearchTreeAfterInsert.search(12) should contain("Jane")
+    binarySearchTreeAfterInsert.search(20) should contain("Sarah")
+    binarySearchTreeAfterInsert.search(14) should contain("Peter")
+    binarySearchTreeAfterInsert.search(17) should contain("Joe")
+    binarySearchTreeAfterInsert.search(34) shouldEqual None
   }
 
 
   "A BinarySearchTree" should "output overwrite if already exits" in {
-    //val binarySearchTree:BinarySearchTree[Int,String] = BinarySearchTree(5,"James")
+    //val binarySearchTree: BinarySearchTree[Int, String] = UnBalancedBinarySearchTree(5, "James")
     val binarySearchTree:BinarySearchTree[Int,String] = null
-    binarySearchTree.insert(4,"Isabel")
-    binarySearchTree.insert(8,"Michelle")
-    binarySearchTree.insert(10,"Ruth")
-    binarySearchTree.insert(10,"Judith")
-    binarySearchTree.search(10) should contain("Judith")
+    binarySearchTree.insert(4, "Isabel")
+      .insert(8, "Michelle")
+      .insert(10, "Ruth")
+      .insert(10, "Judith")
+      .search(10) should contain("Judith")
   }
 }
