7a3c1767d59d97307fbe52757285d603b4d9319f
cutajarj2 <james.cutajar@blip.pt>
2018-05-10 13:32:15 +0100

    closestDistance specs

---
+++ src/test/scala/com/cutajarjames/scalabyexample/lesson17/ClosestDistance2DSpec.scala
@@ -0,0 +1,24 @@
+package com.cutajarjames.scalabyexample.lesson17
+
+import com.cutajarjames.scalabyexample.lesson15.Point
+import org.scalactic.TolerantNumerics
+import org.scalatest.{FlatSpec, Matchers}
+
+class ClosestDistance2DSpec extends FlatSpec with Matchers {
+  implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.001)
+
+  "The ClosestDistanceBruteSpec" should "output the correct value" in {
+    val points = List(Point(10,10), Point(11,11), Point(20,10), Point(10,20), Point(15,13), Point(13,17))
+    ClosestDistance2D.closestDistance(points) shouldEqual 1.4142
+  }
+
+  it should "output the correct value with a larger list" in {
+    val points = List(Point(11,11), Point(100,100), Point(200,10), Point(70,35), Point(73,43), Point(103,27),
+      Point(201,31), Point(87,92), Point(76,45), Point(32,38), Point(10,79), Point(143,22),
+      Point(131,131), Point(187,20), Point(81,45), Point(67,19), Point(2,45), Point(16,122)
+    )
+    ClosestDistance2D.closestDistance(points) shouldEqual 3.6055
+  }
+
+
+}
